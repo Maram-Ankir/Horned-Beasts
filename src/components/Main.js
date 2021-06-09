66b3ada1a6c6d2155d0fcb333679346293c307a4
Maram-Ankir <maramankir5@gmail.com>
2021-06-09 13:52:06 +0300

    adding files

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import HornedBeast from './HornedBeast';
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
 
 
 
@@ -8,6 +11,7 @@ class Main extends React.Component {
   render() {
     
     return (
+      
 <div>
     {
               this.props.HornedData.map((value) => {
